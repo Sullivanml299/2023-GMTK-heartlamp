345bc63c8aba17a458e24d1b8c6e1aaf59d82bfb
HeartLamp <giftsmakeslaves@gmail.com>
2023-07-07 21:40:32 -0400

    I'm so hungry

--- Assets/FootPlacementBackLegs.cs
+++ Assets/FootPlacementBackLegs.cs
@@ -2,13 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FootPlacement : MonoBehaviour
+public class FootPlacementBackLegs : MonoBehaviour
 {
     public Vector3 currentFootPlace;
     public Vector3 footTarget;
     public RaycastHit hit;
-
-    public Vector3 footAimDir;
 
     public float stepSpd, stepDistance;
 
@@ -30,38 +28,22 @@ public class FootPlacement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Vertical") > 0.8)
-        {
-            print(Input.GetAxis("Vertical"));
-            footAimDir = transform.forward; //Random.Range(0.5f,2);
-        }
-        else if (Input.GetAxis("Vertical") < -0.8)
-        {
-            print(Input.GetAxis("Vertical"));
-            footAimDir = -transform.forward; //Random.Range(0.5f, 2);
-        }
-        else
-        {
-            footAimDir = Vector3.zero;
-        }
-
-
-            if (Physics.Raycast(transform.position, -transform.up + footAimDir, out hit, 3.0f))
+        if (Physics.Raycast(transform.position, -transform.up + (-transform.forward / Random.RandomRange(1, 2)), out hit, 3.0f))
         {
             footTarget = hit.point;
         }
 
         if (Vector3.Distance(currentFootPlace, footTarget) > stepDistance)
         {
-            
+
             moveTwd = true;
         }
 
         if (moveTwd == true && Vector3.Distance(currentFootPlace, footTarget) > 0.01)
         {
             var step = stepSpd * Time.deltaTime; // calculate distance to move
-            currentFootPlace =  Vector3.MoveTowards(currentFootPlace, footTarget, step);
-            
+            currentFootPlace = Vector3.MoveTowards(currentFootPlace, footTarget, step);
+
         }
         else
         {
@@ -74,7 +56,7 @@ public class FootPlacement : MonoBehaviour
 
     void OnDrawGizmosSelected()
     {
-        
+
         Gizmos.color = Color.yellow;
         Gizmos.DrawSphere(currentFootPlace, 0.1f);
 
